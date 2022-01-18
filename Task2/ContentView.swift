c0c662e6577b15d232cf4a471d2c282a6d676a6a
dostonru <rustamovdoston602@gmail.com>
2022-01-19 01:05:37 +0500

    Initial Commit

---
+++ Task2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Task2
+//
+//  Created by Doston Rustamov on 19/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task2/Task2App.swift
@@ -0,0 +1,17 @@
+//
+//  Task2App.swift
+//  Task2
+//
+//  Created by Doston Rustamov on 19/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Task2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
